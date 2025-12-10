ab32352b0ba2b51a1e6fbfd007ca53b3c0620cbc
Julian Koripamo <jkoripamo@gmail.com>
2025-12-10 14:33:24 +0100

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
-import Dashboard from './components/Dashboard/Dashboard'
-import Sidebar from './components/Sidebar/Sidebar'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
+  // return (
+  //   <main className='bg-stone-100 grid gap-4 p-4 grid-cols-[220px_1fr]'>
+  //     <Sidebar />
+  //     <Dashboard />
+  //   </main>
+  // )
+
   return (
-    <main className='bg-stone-100 grid gap-4 p-4 grid-cols-[220px_1fr]'>
-      <Sidebar />
-      <Dashboard />
-    </main>
+    <AppRoutes />
   )
 }
 
