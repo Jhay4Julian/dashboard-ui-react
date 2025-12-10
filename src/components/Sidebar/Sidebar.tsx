693e88d36dbb9836c5d91ab01a03a57c30feda58
Julian Koripamo <jkoripamo@gmail.com>
2025-12-10 14:30:43 +0100

    updated sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,14 +1,18 @@
 import AccountToggle from "./AccountToggle"
+import Plan from "./Plan"
+import RouteSelect from "./RouteSelect"
 import Search from "./Search"
 
 const Sidebar = () => {
     return (
         <div>
-            <div className="overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]">
+            <div className="overflow-y-hidden sticky top-4 h-[calc(100vh-32px-48px)]">
                 <AccountToggle />
                 <Search />
+                <RouteSelect />
             </div>
 
+            <Plan />
         </div>
     )
 }
