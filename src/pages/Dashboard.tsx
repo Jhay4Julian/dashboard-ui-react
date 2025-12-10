d0b88da414c358bd2146ca781055a1639b6bc251
Julian Koripamo <jkoripamo@gmail.com>
2025-12-10 14:33:08 +0100

    dashboard page

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,13 @@
+import Grid from "../components/Dashboard/Grid"
+import TopBar from "../components/Dashboard/TopBar"
+
+const Dashboard = () => {
+    return (
+        <div className="">
+            <TopBar />
+            <Grid />
+        </div>
+    )
+}
+
+export default Dashboard
