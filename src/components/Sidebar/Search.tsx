97484d7912993ccc897c84c7ecced8e12f654150
Julian Koripamo <jkoripamo@gmail.com>
2025-12-10 14:29:47 +0100

    updated search

--- src/components/Sidebar/Search.tsx
+++ src/components/Sidebar/Search.tsx
@@ -1,12 +1,29 @@
+import { useState } from "react"
+import { FiCommand, FiSearch } from "react-icons/fi"
+import CommandMenu from "./CommandMenu"
+
 const Search = () => {
+    const [open, setOpen] = useState(false)
+
     return (
         <>
             <div className="bg-stone-200 mb-4 relative rounded flex items-center px-2 py-1.5 text-sm">
-                <input 
-                type="text"
-                placeholder="Search"
-                className="w-full bg-transparent placeholder:text-stone-400 focus:outline-none"
+                <FiSearch className="text-stone-700 mr-2" />
+                <input
+                    onFocus={(e) => {
+                        e.target.blur();
+                        setOpen(true);
+                    }}
+                    type="text"
+                    placeholder="Search"
+                    className="w-full bg-transparent placeholder:text-stone-400 focus:outline-none"
                 />
+
+                <span className="p-1 text-xs flex gap-0.5 items-center bg-stone-50 rounded absolute right-1.5 top-1/2 -translate-y-1/2">
+                    <FiCommand />K
+                </span>
+
+                <CommandMenu open={open} setOpen={setOpen} />
             </div>
         </>
     )
